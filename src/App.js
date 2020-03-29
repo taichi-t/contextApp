dc2593d8c2c684f34298b7ecce7f7428f286467c
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2020-03-29 12:00:00 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./Components/Navbar";
+import BookList from "./Components/BookList";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <BookList />
+    </div>
+  );
+}
+
+export default App;
