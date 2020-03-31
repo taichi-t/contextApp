f71c3c0bf666e66a6787e001897f322a1e995165
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2020-03-31 12:00:00 +0900

    set ThemeContextProvider

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -1,15 +1,18 @@
-import React, { createContext } from "react";
+import React, { createContext, Component } from "react";
 
 export const ThemeContext = createContext();
 
-export default class ThemeContext extends Component {
+export default class ThemeContextProvider extends Component {
   state = {
     isLightTheme: true,
     light: { syntax: "#555", ui: "#ddd", bg: "#eee" },
     dark: { syntax: "#ddd", ui: "#333", bg: "#555" },
   };
   render() {
-    return;
-    <ThemeContext.Provider value={{ ...this.state }}></ThemeContext.Provider>;
+    return (
+      <ThemeContext.Provider value={{ ...this.state }}>
+        {this.props.children}
+      </ThemeContext.Provider>
+    );
   }
 }
