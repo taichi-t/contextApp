dc15860db496a7671ce1903192232db6c0edaca3
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2020-04-12 12:00:00 +0900

    added: hooks

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,27 +1,19 @@
-import React, { Component } from "react";
+import React, { useContext } from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
 
-export default class BookList extends Component {
-  render() {
-    return (
-      <ThemeContext.Consumer>
-        {(context) => {
-          const { isLightTheme, light, dark } = context;
-          const theme = isLightTheme ? light : dark;
-          return (
-            <div
-              className="book-list"
-              style={{ color: theme.syntax, background: theme.bg }}
-            >
-              <ul>
-                <li style={{ background: theme.ui }}>the way of kigs</li>
-                <li style={{ background: theme.ui }}>the name of the wind</li>
-                <li style={{ background: theme.ui }}>the final empire</li>
-              </ul>
-            </div>
-          );
-        }}
-      </ThemeContext.Consumer>
-    );
-  }
+export default function BookList() {
+  const { isLightTheme, light, dark } = useContext(ThemeContext);
+  const theme = isLightTheme ? light : dark;
+  return (
+    <div
+      className="book-list"
+      style={{ color: theme.syntax, background: theme.bg }}
+    >
+      <ul>
+        <li style={{ background: theme.ui }}>the way of kigs</li>
+        <li style={{ background: theme.ui }}>the name of the wind</li>
+        <li style={{ background: theme.ui }}>the final empire</li>
+      </ul>
+    </div>
+  );
 }
